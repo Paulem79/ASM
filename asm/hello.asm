56a42354a2f722f1761f500938f077ef9e2848c8
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2026-03-19 09:11:57 +0100

    feat: enhanced asm code

--- asm/hello.asm
+++ asm/hello.asm
@@ -4,39 +4,47 @@ BITS 64
 %define SYS_EXIT 60
 
 section .data
-    msg1 db 'Hello, World!', 0xA
-    len1 equ $ - msg1
-    
-    msg2 db 'Assembleur est puissant.', 0xA
-    len2 equ $ - msg2
+    ; IMPORTANT : Ajout du , 0 à la fin pour marquer la fin de la chaîne (null-terminated)
+    msg1 db 'Hello, World!', 0xA, 0
+    msg2 db 'Assembleur est puissant.', 0xA, 0
 
 section .text
     global _start
 
 _start:
-    ; --- Afficher le premier message ---
-    mov rdi, msg1       ; Arg 1 : Adresse de la chaîne
-    mov rsi, len1       ; Arg 2 : Longueur
+    ; Afficher le premier message
+    mov rdi, msg1
     call _print
 
-    ; --- Afficher le deuxième message ---
+    ; Afficher le deuxième message
     mov rdi, msg2
-    mov rsi, len2
     call _print
 
-    ; Exit
+    ; Quitter
     mov rax, SYS_EXIT
     xor rdi, rdi
     syscall
 
 ; ---------------------------------------------------------
-; _print : Affiche une chaîne de caractères
-; Entrée : RDI = adresse du message, RSI = longueur
+; _print : Calcule la longueur et affiche la chaîne
+; Entrée : RDI = adresse de la chaîne (doit finir par 0)
 ; ---------------------------------------------------------
 _print:
-    mov rdx, rsi        ; On déplace la longueur dans RDX (requis par syscall)
-    mov rsi, rdi        ; On déplace l'adresse dans RSI (requis par syscall)
+    ; 1. Calcul de la longueur
+    mov rsi, rdi        ; On garde l'adresse de départ dans RSI pour le futur syscall
+    xor rdx, rdx        ; RDX servira de compteur (longueur)
+
+.count_loop:
+    cmp byte [rdi + rdx], 0 ; Vérifie le caractère à l'adresse (RDI + RDX)
+    je .count_done          ; Si c'est 0, on a fini
+    inc rdx                 ; Sinon, longueur++
+    jmp .count_loop
+
+.count_done:
+    ; 2. Appel système SYS_WRITE
+    ; RDX contient déjà la longueur calculée
+    ; RSI contient déjà l'adresse du message
     mov rax, SYS_WRITE
-    mov rdi, 1          ; File Descriptor (stdout)
+    mov rdi, 1              ; File Descriptor 1 (stdout)
     syscall
     ret
